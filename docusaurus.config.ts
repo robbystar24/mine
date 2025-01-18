aab979de24dcb935e1ecd7665c643a94f38084ed
robbystar24 <jjisu1019@gmail.com>
2025-01-18 07:33:51 +0000

    ㅅ

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     favicon: 'img/favicon.ico',
 
     // TODO( github project link )
-    url: 'https://seonWKim.github.io',
+    url: 'https://robbystar24.github.io',
     // TODO (github project name e.g. /simple-web-programming/ )
     baseUrl: '/mine/',
 
